f4679aabcbc0daecc45092300b6b7198b9781237
DzakwanGoreng <161908992+DzakwanGoreng@users.noreply.github.com>
2025-03-13 23:00:58 +0700

    Delete add_order.php

--- add_order.php
+++
@@ -1,8 +0,0 @@
-<?php include 'db.php'; 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $customer_id = $_POST['customer_id'];
-    $order_date = $_POST['order_date'];
-    $stmt = $pdo->prepare("INSERT INTO orders (customer_id, order_date) VALUES (?, ?)");
-    $stmt->execute([$customer_id, $order_date]);
-    header("Location: index.php");
-}
