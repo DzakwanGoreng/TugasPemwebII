b535a33252de8b340eb0ea01230c262b0e7e3938
DzakwanGoreng <161908992+DzakwanGoreng@users.noreply.github.com>
2025-03-13 23:03:31 +0700

    Delete delete_order.php

--- delete_order.php
+++
@@ -1,7 +0,0 @@
-<?php include 'db.php'; 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $stmt = $pdo->prepare("DELETE FROM orders WHERE id = ?");
-    $stmt->execute([$id]);
-    header("Location: index.php");
-}
